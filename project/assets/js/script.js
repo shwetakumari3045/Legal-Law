61c4bd5ac39f59efead50a49d0fe469748f17da3
shwetakumari3045 <162788159+shwetakumari3045@users.noreply.github.com>
2025-04-22 21:32:29 +0530

    https://github.com/shwetakumari3045/Legal-Law.git

---
+++ project/assets/js/script.js
@@ -0,0 +1,17 @@
+$('.document-slider').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    infinite: true,
+    arrows:true,
+    prevArrow:`<button type="button" class="slick-prev my-arrow">Previous</button>`,
+    nextArrow:`<button type="button" class="slick-next my-arrow">Next</button>`
+  });
+
+  $('.testimonial-slider').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    infinite: true,
+    arrows:true,
+  });
